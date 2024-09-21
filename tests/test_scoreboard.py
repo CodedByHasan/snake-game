cb6fe754e04dfe54e54f960fd3efe30074804171
Hasan <alkhashaab.h@gmail.com>
2024-09-21 18:30:50 +0930

    test: added more unit tests for scoreboard class

--- tests/test_scoreboard.py
+++ tests/test_scoreboard.py
@@ -16,12 +16,20 @@ def test_scoreboard_init(scoreboard):
     assert scoreboard.color() == ("white", "white")
     assert scoreboard.speed() == 0 # fastest
     assert scoreboard.pos() == (0,260)
+    assert scoreboard.isvisible() == False
 
 
-def test_score_increase(scoreboard):
+def test_increase_score(scoreboard):
     initial_score = scoreboard.score
     scoreboard.increase_score()
 
     assert scoreboard.score == initial_score + 1
 
+def test_update_scoreboard(scoreboard):
+    with patch.object(scoreboard, 'clear') as mock_clear, \
+         patch.object(scoreboard, 'write') as mock_write:
 
+        scoreboard.update_scoreboard()
+
+        mock_clear.assert_called_once()
+        mock_write.assert_called_once_with("Score: 0", align="center", font=("Arial", 24, "normal"))
