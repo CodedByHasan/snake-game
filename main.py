0b39bf699f50fa01a03434cfd57f625659068f14
Hasan <alkhashaab.h@gmail.com>
2024-09-21 17:17:23 +0930

    feat: refactored snake game into module

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from turtle import Screen
 import time
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
+import sys
+
+sys.path.append('./src')
+
+from snake_game.snake import Snake
+from snake_game.food import Food
+from snake_game.scoreboard import Scoreboard
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
@@ -22,7 +26,6 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 game_is_on = True
-
 
 while game_is_on:
     screen.update()
