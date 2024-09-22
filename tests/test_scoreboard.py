f1b292b7300513152b5174dd0427ef12c3cd7456
Hasan <alkhashaab.h@gmail.com>
2024-09-22 17:03:36 +0930

    chore: formatting and fixed sys path so that test can be run from root

--- tests/test_scoreboard.py
+++ tests/test_scoreboard.py
@@ -1,8 +1,9 @@
 import sys
-import pytest
+import os
 from unittest.mock import patch
+import pytest
 
-sys.path.append('../src')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 from snake_game.scoreboard import Scoreboard
 
@@ -11,12 +12,13 @@ from snake_game.scoreboard import Scoreboard
 def scoreboard():
     return Scoreboard()
 
+
 def test_scoreboard_init(scoreboard):
     assert scoreboard.score == 0
     assert scoreboard.color() == ("white", "white")
-    assert scoreboard.speed() == 0 # fastest
-    assert scoreboard.pos() == (0,260)
-    assert scoreboard.isvisible() == False
+    assert scoreboard.speed() == 0  # fastest
+    assert scoreboard.pos() == (0, 260)
+    assert scoreboard.isvisible() is False
 
 
 def test_increase_score(scoreboard):
@@ -25,6 +27,7 @@ def test_increase_score(scoreboard):
 
     assert scoreboard.score == initial_score + 1
 
+
 def test_update_scoreboard(scoreboard):
     with patch.object(scoreboard, 'clear') as mock_clear, \
          patch.object(scoreboard, 'write') as mock_write:
@@ -32,4 +35,6 @@ def test_update_scoreboard(scoreboard):
         scoreboard.update_scoreboard()
 
         mock_clear.assert_called_once()
-        mock_write.assert_called_once_with("Score: 0", align="center", font=("Arial", 24, "normal"))
+        mock_write.assert_called_once_with(
+            "Score: 0", align="center", font=("Arial", 24, "normal")
+        )
