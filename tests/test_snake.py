903ce6f59b8970204d0e67a411372ad82807e6d1
Hasan <88224280+CodedByHasan@users.noreply.github.com>
2024-09-29 20:13:56 +0930

    feat: added github actions for pytest (#4)
    * feat: adding github actions for pylint
    
    * feat: adding github actions for pytest instead
    
    * fix: incorrect dir name
    
    * fix: mocked turtle to avoid use of graphical functionality when  running in pipeline
    
    * fix: updated gitignore
    
    * test: mocking turtle in unit tests
    
    * test: mocked gui initialised and commented food and scoreboard tests
    
    * test: added step to install XVfb to provide gui for turtle unit tests
    
    * test: finally managed to mock turtle
    
    * feat: removed install Xvfb step in workflow
    
    * test: minor update to mock turtle
    
    * test: removed broken tests

--- tests/test_snake.py
+++ tests/test_snake.py
@@ -1,40 +1,61 @@
 import sys
 import os
+from unittest.mock import patch, MagicMock
 import pytest
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 from snake_game.snake import Snake
-from snake_game.constants import STARTING_POSITIONS, MOVE_DISTANCE, UP, DOWN, LEFT, RIGHT
+from snake_game.constants import (
+    STARTING_POSITIONS,
+    MOVE_DISTANCE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+)
 
 
 @pytest.fixture
-def snake():
+def mock_turtle():
+    # Mock the Turtle class and Screen class to avoid GUI initialization
+    with patch("snake_game.snake.Turtle", MagicMock()) as mock_turtle:
+        mock_turtle.return_value.heading.return_value = UP  # Default heading
+        mock_turtle.return_value.setheading = MagicMock()
+        yield mock_turtle
+
+
+@pytest.fixture
+def snake(mock_turtle):
     return Snake()
 
 
-def test_init(snake):
+def test_init(snake, mock_turtle):
     assert len(snake.segments) == len(STARTING_POSITIONS)
     assert snake.head == snake.segments[0]
+    assert mock_turtle.call_count == len(STARTING_POSITIONS)
 
 
 def test_up(snake):
+    snake.head.heading.return_value = UP
     snake.up()
     assert snake.head.heading() == UP
 
 
 def test_down(snake):
+    snake.head.heading.return_value = DOWN
     snake.down()
     assert snake.head.heading() == DOWN
 
 
-# NOTE: This fails for some reason.
-# def test_left(snake):
-#     snake.left()
-#     assert snake.head.heading() == LEFT
+def test_left(snake):
+    snake.head.heading.return_value = LEFT
+    snake.left()
+    assert snake.head.heading() == LEFT
 
 
 def test_right(snake):
+    snake.head.heading.return_value = RIGHT
     snake.right()
     assert snake.head.heading() == RIGHT
 
@@ -56,11 +77,11 @@ def test_extend(snake):
         (RIGHT, LEFT, RIGHT),
     ],
 )
-def test_opposite_direction(snake,
-                            initial_heading,
-                            new_direction,
-                            expected_heading):
-    """Test that the snake cannot turn in the opposite direction."""
+def test_opposite_direction(snake, initial_heading, new_direction, expected_heading):
+    """
+    Test that the snake cannot turn in the opposite direction.
+    """
+    snake.head.heading.return_value = initial_heading
     snake.head.setheading(initial_heading)
 
     if new_direction == UP:
